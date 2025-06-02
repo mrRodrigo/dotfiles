c498503d7bfe9b5cf164d570b76809b88553ada4
mrrodrigo <rodrigo.mello72@hotmail.com>
2025-06-02 16:14:25 -0300

    tmux config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+
+config.font = wezterm.font("Hack Nerd Font Mono")
+config.font_size = 15
+
+
+config.enable_tab_bar = false
+--config.window_decorations = "RESIZE"
+
+
+return config
